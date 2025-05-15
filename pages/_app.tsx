a72451d951d0c85f297a1f76c6f9b7a8a1d41938
AXIA-Enterprises <axia@0020.org>
2025-05-15 19:14:35 +0000

    update app.tx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { WalletConnectionProvider } from '@/components/WalletConnectionProvider';
+import { WalletConnectionProvider } from '../components/WalletConnectionProvider';
+
+// Import wallet adapter styles
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
